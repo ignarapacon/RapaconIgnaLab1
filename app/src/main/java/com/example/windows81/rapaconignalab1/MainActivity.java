19a8e68b064e1a1451d07996ba3f6f82d8ece131
Igna Rapacon <2015081257@ust-ics.mygbiz.com>
2018-09-04 14:58:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/windows81/rapaconignalab1/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.windows81.rapaconignalab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
